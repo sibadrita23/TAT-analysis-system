de50762acdb17ea4a35f1a978bbda2702032773b
SHIBADRITA SAHA <152030215+sibadrita23@users.noreply.github.com>
2024-08-19 11:51:56 +0530

    Update login.js

--- touch rpa_scripts/login.js
+++ touch rpa_scripts/login.js
@@ -1,1 +1,17 @@
 touch rpa_scripts/login.js
+const puppeteer = require('puppeteer');
+
+async function login(url, username, password) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  
+  await page.goto(url);
+  
+  await page.type('#username', username);
+  await page.type('#password', password);
+  await page.click('#login-button');
+  
+  await browser.close();
+}
+
+module.exports = login;
